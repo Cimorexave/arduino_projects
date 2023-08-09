ca140c3fb704bfdeab943960be4e233489aaa21f
Cimorexave <sadeq.s502@gmail.com>
2023-08-09 19:15:34 +0330

    modified sensitivity and update rate

--- analog_light/analog_light.ino
+++ analog_light/analog_light.ino
@@ -7,12 +7,13 @@ void setup() {
 }
 
 void loop() {
-  delay(0.5 * SECOND);
+  // delay(0.5 * SECOND);
 
   int sensor_value = analogRead(A0);
   
   Serial.print("light sensor analog input value ( x / 1023):  ");
   Serial.println(sensor_value);
 
-  analogWrite(LED_OUT, sensor_value / 4);
+  // analogWrite(LED_OUT, sensor_value / 4);
+  analogWrite(LED_OUT, sensor_value);
 }
